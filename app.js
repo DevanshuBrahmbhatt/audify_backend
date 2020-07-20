af08ad375cf0cfbe4f32e372b340fd3cd36a920e
devanshu <devanshu.vguj@gmail.com>
2020-07-20 09:30:20 +0530

    Database connected name is sdp_db

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var path = require('path');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+const port = 5000;
+app.set('port', process.env.port || port);
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+// const routes = require('./routes/api/team');
+// app.use('/api', routes);
+
+// DB Connection Done
+var db = require('./config/conn').url;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
---
+++ config/conn.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url:
+    'mongodb+srv://sdp:sdp1128@cluster0.yfxsu.mongodb.net/sdp_db?retryWrites=true&w=majority',
+  secret: 'sdp4project%$#1128',
+};
